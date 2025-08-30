e971b63290fcbd91d50eafb699c63afb7e7208b2
fideris susanto <fsus8677@uni.sydney.edu.au>
2025-08-30 17:02:00 +1000

    created the frontend

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -8,25 +8,27 @@ import handImage from '../assets/hand.png'; // Ensure you have this image in the
 const HomePage = () => {
   return (
     <section className="homepage">
+        <div className="home__container container grid">
       
-        <div className="homepage__image">
-            <img src={earthImage} alt="Earth" className="homepage__earth" />
-            <img src={handImage} alt="Hand" className="homepage__hand" />
-        </div>
+            <div className="homepage__image">
+                <img src={earthImage} alt="Earth" className="homepage__earth" />
+                <img src={handImage} alt="Hand" className="homepage__hand" />
+            </div>
 
-        <div className="homepage__tagline">
-            <h1 className="homepage__title">Scan. Sort.</h1>
-            <h1 className="homepage__title">Save the PLANET.</h1>
-            
-            <p className="homepage__vision">
-                Your camera. Our AI. A planet-first solution to waste sorting.
-            </p>
+            <div className="homepage__tagline">
+                <h1 className="homepage__title">Scan. Sort.</h1>
+                <h1 className="homepage__title">Save the PLANET.</h1>
+                
+                <p className="homepage__vision">
+                    Your camera. Our AI. A planet-first solution to waste sorting.
+                </p>
 
-            <div className="homepage__buttons">
-                <button className="homepage__button">
-                    <img src={nextIcon} alt="Next Icon" style={{ width: '20px', height: '20px', marginRight: '8px' }} />
-                    Scan now!
-                </button>
+                <div className="homepage__buttons">
+                    <button className="homepage__button">
+                        <img src={nextIcon} alt="Next Icon" style={{ width: '20px', height: '20px', marginRight: '8px' }} />
+                        Scan now!
+                    </button>
+                </div>
             </div>
         </div>
     </section>
